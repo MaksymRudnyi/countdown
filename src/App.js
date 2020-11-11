104e62e9f38ce569f5d5bdd822f5503807e28775
Maksym Rudnyi <maksym.rudnyi@fiverr.com>
2020-11-11 23:23:31 +0200

    super countdown

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react';
+import './app.style.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [repaintClass, setRepaintClass] = useState('');
+    const [second, setSecond] = useState(3);
+    let grid = [];
+
+    useEffect(() => {
+        if (second < -1) {
+            return;
+        }
+
+        const timer1 = setTimeout(() => {
+            setSecond( second - 1);
+        }, 2000);
+
+        const timer2 = setTimeout(() => {
+            setRepaintClass(`repaint`);
+        }, 1500);
+
+        return () => {
+            clearTimeout(timer1);
+            clearTimeout(timer2);
+        }
+    });
+
+    useEffect(() => setRepaintClass(`number-${second}`), [second]);
+
+    for (let x = 1; x <= 15; x ++) {
+        for (let y = 1; y <= 9; y++) {
+            grid.push(<div className={`x${x}-y${y}`}></div>);
+        }
+    }
+
+    if (second <= -1) {
+        return (
+            <div>
+                <h1>Hello my app</h1>
+                <button onClick={() => setSecond(3)}>Start over</button>
+            </div>
+
+        )
+    }
+
+    return (
+        <div className="countdown">
+            <div id="countdown-grid" className={repaintClass}>
+                {grid}
+            </div>
+        </div>
+    )
+};
 
 export default App;
